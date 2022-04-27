dce57b357c3f3d43d4c82df27971bf22de8b32ec
Marco Crippa <xmarcocrippa@gmail.com>
2022-04-27 13:40:01 -0300

    Function remove.py complete

--- remove.py
+++ remove.py
@@ -1,50 +1,105 @@
 import time
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-def remover:
+from cache import cache
 
-    numero = str(input('Digite o numero para ser removido'))
-    states = ['CA', 'FL', 'MA', 'NY']
+# Define variables
 
-    for s in states:
+whtasapp = "https://web.whatsapp.com/"
+
+numero = str(input('Digite o numero para ser removido: '))
+
+states = ['CA', 'FL', 'MA', 'NY']
+
+nincho = ['Empregos', 'Apartments', 'Carros', 'Delivery',
+          'Doação', 'Mercadão', 'Seguros', 'Comidas BR']
+
+# Open WebBrowser
+s = Service('.\chromedriver.exe')  # webdriver path
+driver = webdriver.Chrome(service=s, options=cache.options)
+
+
+# Get URL
+driver.get(whtasapp)
+time.sleep(20)  # Sleep 20 seconds beacuse some times the load takes too long
+
+# Search for a number inside all groups
+
+for s in states:
+    for i in nincho:
         for n in range(1, 4):
+
             # Search for groups
             driver.find_element_by_xpath(
-                '//div[@title="Search input textbox"]').send_keys(f'BNA - Empregos {n} 🇺🇸🇧🇷 {s}')
+                '//div[@title="Search input textbox"]').send_keys(f'BNA - {i} {n}')
             time.sleep(2)
 
-            # Click on describe group
-            driver.find_element_by_xpath(
-                f'//span[@title="BNA - Empregos {n} 🇺🇸🇧🇷 {s}"]').click()
-            time.sleep(1)
+            try:
+                # Click on describe group
+                driver.find_element_by_xpath(
+                    f'//span[@title="BNA - {i} {n} 🇺🇸🇧🇷 {s}"]').click()
+                time.sleep(1.5)
 
-            # Open de group menu
-            driver.find_elements_by_xpath(
-                f'//span[@title="BNA - Empregos {n} 🇺🇸🇧🇷 {s}"]')[1].click()
-            time.sleep(1)
+            except:
+                # Clear the search input text
+                driver.find_element_by_xpath(
+                    '//div[@title="Search input textbox"]').clear()
+                time.sleep(2)
 
-            # Click on search icon in group menu
-            driver.find_elements_by_xpath(
-                '//span[@data-icon="search"]')[0].click()
-            time.sleep(1)
+            else:
+                # Open de group menu
+                driver.find_elements_by_xpath(
+                    f'//span[@title="BNA - {i} {n} 🇺🇸🇧🇷 {s}"]')[1].click()
+                time.sleep(1.5)
 
-            # Search for participant
-            driver.find_elements_by_xpath(
-                '//div[@title="Search input textbox"]')[0].send_keys(f'{numero}')
-            time.sleep(1)
+                # Click on search icon in group menu
+                driver.find_elements_by_xpath(
+                    '//span[@data-icon="search"]')[0].click()
+                time.sleep(1.5)
 
-            # Click on the frist number on menu
-            driver.find_elements_by_xpath('//div[@tabindex="0"]')[0].click()
-            time.sleep(1)
+                # Search for participant
+                driver.find_elements_by_xpath(
+                    '//div[@title="Search input textbox"]')[0].send_keys(f'{numero}')
+                time.sleep(1.5)
 
-            # Remove the number of the group
-            driver.find_element_by_xpath('//div[@aria-label="Remove"]').click()
-            time.sleep(1)
+                try:
+                    # Click on the frist number on menu
+                    driver.find_elements_by_xpath(
+                        '//div[@tabindex="0"]')[0].click()
+                    time.sleep(1.5)
 
-            # Close the Search Participants
-            driver.find_element_by_xpath('//span[@data-testid="x"]').click()
-            time.sleep(1)
+                except:
+                    # Close the Search Participants
+                    driver.find_element_by_xpath(
+                        '//span[@data-testid="x"]').click()
+                    time.sleep(1.5)
 
-            # Close the Group menu
-            driver.find_element_by_xpath('//span[@data-testid="x"]').click()
-            time.sleep(5)
+                    # Clear the search input text
+                    driver.find_element_by_xpath(
+                        '//div[@title="Search input textbox"]').clear()
+                    time.sleep(1.5)
+
+                else:
+                    # Remove the number of the group
+                    driver.find_element_by_xpath(
+                        '//div[@aria-label="Remove"]').click()
+                    time.sleep(1.5)
+
+                    # Close the Search Participants
+                    driver.find_element_by_xpath(
+                        '//span[@data-testid="x"]').click()
+                    time.sleep(1.5)
+
+                    # Close the Group menu
+                    driver.find_element_by_xpath(
+                        '//span[@data-testid="x"]').click()
+                    time.sleep(1.5)
+
+                    # Clear the search input text
+                    driver.find_element_by_xpath(
+                        '//div[@title="Search input textbox"]').clear()
+                    time.sleep(1.5)
+
+driver.quit()
--- teste.py
+++ teste.py
@@ -5,60 +5,18 @@ from selenium.webdriver.chrome.service import Service
 
 from cache import cache
 
-# Define URL
+# Define variables
 
 whtasapp = "https://web.whatsapp.com/"
+
 
 # Open WebBrowser
 s = Service('.\chromedriver.exe')  # webdriver path
 driver = webdriver.Chrome(service=s, options=cache.options)
 
 
-# Get URL
+# Get URL11951569460
 driver.get(whtasapp)
-time.sleep(10)  # Sleep 10 seconds beacuse some times the load takes too long
+time.sleep(20)  # Sleep 20 seconds beacuse some times the load takes too long
 
-# Search for groups
-driver.find_element_by_xpath(
-    '//div[@title="Search input textbox"]').send_keys(f'BNA - Empregos {b}')
-
-time.sleep(2)
-
-# Click on describe group
-driver.find_element_by_xpath(
-    f'//span[@title="BNA - Empregos {n} 🇺🇸🇧🇷 CA"]').click()
-time.sleep(1)
-
-# Open de group menu
-driver.find_elements_by_xpath(
-    f'//span[@title="BNA - Empregos {n} 🇺🇸🇧🇷 NY"]')[1].click()
-time.sleep(1)
-
-# Click on search icon in group menu
-driver.find_elements_by_xpath('//span[@data-icon="search"]')[0].click()
-time.sleep(1)
-
-# Search for participant
-driver.find_elements_by_xpath(
-    '//div[@title="Search input textbox"]')[0].send_keys(f'{numero}')
-time.sleep(1)
-
-# Click on the frist number on menu
-driver.find_elements_by_xpath('//div[@tabindex="0"]')[0].click()
-time.sleep(1)
-
-# Remove the number of the group
-driver.find_element_by_xpath('//div[@aria-label="Remove"]').click()
-time.sleep(1)
-
-# Close the Search Participants
-driver.find_element_by_xpath('//span[@data-testid="x"]').click()
-time.sleep(1)
-
-# Close the Group menu
-driver.find_element_by_xpath('//span[@data-testid="x"]').click()
-time.sleep(5)
-
-
-# Close the webdriver
-driver.quit()
+# Search for a number inside all groups
